390e676bffcacb02ef1db7c93e7760ee008b5479
Emil Vaklinov <emil_vaklinov@yahoo.com>
2021-02-05 16:06:05 +0000

    First API call completed and populate Peoples data

--- StarWarsApp/View/PeopleDetailVC.swift
+++ StarWarsApp/View/PeopleDetailVC.swift
@@ -22,3 +22,23 @@ class PeopleDetailVC: UIViewController {
     }
 }
 
+extension PeopleDetailVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterDetail",
+                                                 for: indexPath)
+        if let type = viewModel?.rowDetails(for: indexPath) {
+            cell.textLabel?.text = type.description
+            cell.detailTextLabel?.text = type.value
+        }
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.rowCount(for: section) ?? 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel?.sectionCount ?? 0
+    }
+}
--- StarWarsApp/ViewModel/PeopleDetailVM.swift
+++ StarWarsApp/ViewModel/PeopleDetailVM.swift
@@ -24,4 +24,88 @@ class PeopleDetailVM {
     init(with model: PeopleDataModel) {
         self.model = model
     }
+
+private lazy var sections: [SectionType] = {
+    var sections: [SectionType] = [.attributes]
+    model?.films.forEach{ _ in sections.append(.films) }
+    return sections
+}()
+
+var sectionCount: Int {
+    return sections.count
+}
+
+func rowCount(for index: Int) -> Int {
+    switch sectionType(for: index) {
+    case .attributes:
+        return attributeRows.count
+    case .films:
+        return singleFilmRows.count
+    }
+}
+
+private var attributeRows: [RowType] {
+    let rows: [RowType] = [
+                            .name, .birthYear, .height, .mass, .hairColor,
+                            .skinColor, .eyeColor, .gender
+                          ]
+    return rows
+}
+
+private var singleFilmRows: [RowType] {
+    let rows: [RowType] = [.film, .crawlCharacter]
+    return rows
+}
+
+private func sectionType(for index: Int) -> SectionType {
+    return sections[index]
+}
+
+private func rowType(for index: IndexPath) -> RowType {
+    switch sectionType(for: index.section) {
+    case .attributes:
+        return attributeRows[index.row]
+    case .films:
+        return singleFilmRows[index.row]
+    }
+}
+
+func rowDetails(for index: IndexPath) -> (description: String, value: String?) {
+    
+    let section = sectionType(for: index.section)
+    let row = rowType(for: index)
+    
+    switch (section, row) {
+    case (.attributes, .name):
+        return ("Name".localized(), model?.people?.name)
+    case (.attributes, .birthYear):
+        return ("Birth Year".localized(), model?.people?.birthYear)
+    case (.attributes, .height):
+        return ("Height".localized(), model?.people?.height)
+    case (.attributes, .mass):
+        return ("Mass".localized(), model?.people?.mass)
+    case (.attributes, .hairColor):
+        return ("Hair Color".localized(), model?.people?.hairColor)
+    case (.attributes, .skinColor):
+        return ("Skin Color".localized(), model?.people?.skinColor)
+    case (.attributes, .eyeColor):
+        return ("Eye Color".localized(), model?.people?.eyeColor)
+    case (.attributes, .gender):
+        return ("Gender".localized(), model?.people?.gender.rawValue)
+    case (.films, .film):
+        return ("Film".localized(), film(for: index)?.title)
+    case (.films, .crawlCharacter):
+        return ("Opening crawl".localized(),
+                "\(film(for: index)?.openingCrawl.count ?? 0)")
+    default:
+        return ("", "")
+    }
+}
+
+private func film(for index: IndexPath) -> Film? {
+    guard let films = model?.films else {
+        return nil
+    }
+    return films[index.section - 1]
+}
 }
